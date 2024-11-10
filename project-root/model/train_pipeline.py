003bdaa9e2e1059f102c1094ac385c76badcbd50
Robert Kenzo Medina Monsalve <roberkenzo23@outlook.com>
2024-11-10 18:07:29 -0500

    Adjusting core imports

--- project-root/model/train_pipeline.py
+++ project-root/model/train_pipeline.py
@@ -1,7 +1,7 @@
 import numpy as np
 from imblearn.over_sampling import SMOTE
 from sklearn.model_selection import train_test_split
-from config.core import config
+from model.config.core import app_config, model_config
 from pipeline import diabetes_pipe
 from processing.data_manager import load_dataset, save_pipeline
 
@@ -9,18 +9,18 @@ def run_training() -> None:
     """Entrena y guarda el modelo."""
 
     # Carga de datos de entrenamiento
-    data = load_dataset(file_name=config.app_config.train_data_file)
+    data = load_dataset(file_name=app_config.train_data_file)
     
     # División de datos en entrenamiento y prueba
     X_train, X_test, y_train, y_test = train_test_split(
-        data.drop(columns=config.model_config.target),
-        data[config.model_config.target],
-        test_size=config.model_config.test_size,
-        random_state=config.model_config.random_state,
+        data.drop(columns=model_config.target),
+        data[model_config.target],
+        test_size=model_config.test_size,
+        random_state=model_config.random_state,
     )
 
     # Balanceo con SMOTE
-    sm = SMOTE(random_state=config.model_config.random_state)
+    sm = SMOTE(random_state=model_config.random_state)
     X_train_balanced, y_train_balanced = sm.fit_resample(X_train, y_train)
 
     # Entrena el modelo
