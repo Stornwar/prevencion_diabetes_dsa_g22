daf6bf3d08edebd72e6c28ecc73dd4612a786e03
Robert Kenzo Medina Monsalve <roberkenzo23@outlook.com>
2024-11-10 18:48:20 -0500

    Update data_manager.py

--- project-root/model/processing/data_manager.py
+++ project-root/model/processing/data_manager.py
@@ -3,9 +3,14 @@ import joblib
 from config.core import app_config, model_config
 from model.pipeline import agrupa_edades, agrupa_ingreso
 
-def load_dataset(*, file_name: str) -> pd.DataFrame:
+def load_dataset(*, file_name: str, target_file: str = None) -> pd.DataFrame:
     """Carga un conjunto de datos en formato CSV."""
     data = pd.read_csv(f"{app_config.data_folder}/{file_name}")
+    
+    # Cargar el archivo de la variable objetivo si está separado
+    if target_file:
+        target = pd.read_csv(f"{app_config.data_folder}/{target_file}", names=[model_config.target])
+        data = pd.concat([data, target], axis=1)
     
     # Agrupación de edad e ingreso
     data['Age'] = data['Age'].apply(agrupa_edades)
