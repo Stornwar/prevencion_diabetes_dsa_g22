b8239d5997db82d73effb96e6cf1d00147646d51
Robert Kenzo Medina Monsalve <roberkenzo23@outlook.com>
2024-11-10 22:23:13 -0500

    Update main.py

--- project-root/app/api/main.py
+++ project-root/app/api/main.py
@@ -1,7 +1,7 @@
 from fastapi import FastAPI
 from fastapi.middleware.cors import CORSMiddleware
 from app.api.v1.endpoints import health, predict
-from app.api.config import settings
+from app.api.v1.config import settings
 
 app = FastAPI(
     title=settings.PROJECT_NAME,
