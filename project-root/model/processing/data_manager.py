49496a321f0e40652dbe90bc2307d412586cd1d9
Robert Kenzo Medina Monsalve <roberkenzo23@outlook.com>
2024-11-10 19:30:28 -0500

    Update data_manager.py

--- project-root/model/processing/data_manager.py
+++ project-root/model/processing/data_manager.py
@@ -1,20 +1,17 @@
 import pandas as pd
 import joblib
-from config.core import app_config, model_config
+from model.config.core import app_config, model_config
 from model.pipeline import agrupa_edades, agrupa_ingreso
 
-def load_dataset(*, file_name: str, target_file: str = None) -> pd.DataFrame:
+def load_dataset(*, file_name: str) -> pd.DataFrame:
     """Carga un conjunto de datos en formato CSV."""
     data = pd.read_csv(f"{app_config.data_folder}/{file_name}")
     
-    # Cargar el archivo de la variable objetivo si está separado
-    if target_file:
-        target = pd.read_csv(f"{app_config.data_folder}/{target_file}", names=[model_config.target])
-        data = pd.concat([data, target], axis=1)
-    
-    # Agrupación de edad e ingreso
-    data['Age'] = data['Age'].apply(agrupa_edades)
-    data['Income'] = data['Income'].apply(agrupa_ingreso)
+    # Solo aplica el preprocesamiento si se trata del archivo de características
+    if file_name == app_config.train_data_file:
+        # Agrupación de edad e ingreso
+        data['Age'] = data['Age'].apply(agrupa_edades)
+        data['Income'] = data['Income'].apply(agrupa_ingreso)
     
     return data
 
