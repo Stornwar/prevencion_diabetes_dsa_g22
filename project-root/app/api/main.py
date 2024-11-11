297dbd9fc170618a51ba72a28b629ede1f85e091
Robert Kenzo Medina Monsalve <roberkenzo23@outlook.com>
2024-11-10 22:28:35 -0500

    Update main.py

--- project-root/app/api/main.py
+++ project-root/app/api/main.py
@@ -3,6 +3,10 @@ from fastapi.middleware.cors import CORSMiddleware
 from app.api.v1.endpoints import health, predict
 from app.api.v1.config import settings
 
+@app.get("/")
+async def root():
+    return {"message": "API is running"}
+
 app = FastAPI(
     title=settings.PROJECT_NAME,
     openapi_url=f"{settings.API_V1_STR}/openapi.json"
