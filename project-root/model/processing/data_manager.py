956308e0148fe406f946e4cca10bf04130de5238
Robert Kenzo Medina Monsalve <roberkenzo23@outlook.com>
2024-11-10 19:51:57 -0500

    Update data_manager.py

--- project-root/model/processing/data_manager.py
+++ project-root/model/processing/data_manager.py
@@ -1,8 +1,8 @@
 import os
 import pandas as pd
 import joblib
-from model.config.core import app_config, model_config
-from model.pipeline import agrupa_edades, agrupa_ingreso
+from config.core import app_config, model_config
+from pipeline import agrupa_edades, agrupa_ingreso
 
 def load_dataset(*, file_name: str) -> pd.DataFrame:
     """Carga un conjunto de datos en formato CSV."""
