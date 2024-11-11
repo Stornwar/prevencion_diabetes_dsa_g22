4a44501b5544cd180f6d14359e3e21c10ab94850
Robert Kenzo Medina Monsalve <roberkenzo23@outlook.com>
2024-11-10 22:20:15 -0500

    Update core.py

--- project-root/model/config/core.py
+++ project-root/model/config/core.py
@@ -4,7 +4,7 @@ class AppConfig(BaseSettings):
     data_folder: str = "data"  # Ruta relativa a la carpeta `data` en la raíz del proyecto
     train_data_file: str = "cdc_diabetes_health_indicators_features.csv"
     train_target_file: str = "cdc_diabetes_health_indicators_target.csv"  # Archivo de la variable objetivo
-    pipeline_save_file: str = "artifacts/diabetes_prediction_pipeline"
+    pipeline_save_file: str = "diabetes_prediction_pipeline"
 
 # Configuración para el modelo
 class ModelConfig(BaseSettings):
