1ce0e9b17936b28f953eaeb26a5586fe691ddeb6
Robert Kenzo Medina Monsalve <roberkenzo23@outlook.com>
2024-11-10 19:16:50 -0500

    Update train_pipeline.py

--- project-root/model/train_pipeline.py
+++ project-root/model/train_pipeline.py
@@ -4,6 +4,7 @@ from sklearn.model_selection import train_test_split
 from model.config.core import app_config, model_config
 from model.pipeline import diabetes_pipe
 from model.processing.data_manager import load_dataset, save_pipeline
+from collections import Counter
 
 def run_training() -> None:
     """Entrena y guarda el modelo."""
@@ -26,7 +27,8 @@ def run_training() -> None:
     print(y_train.value_counts())
 
     # Balanceo con SMOTE
-    sm = SMOTE(k_neighbors=1, random_state=model_config.random_state)
+    min_class_count = min(Counter(y_train).values())
+    sm = SMOTE(k_neighbors=min(min_class_count - 1, 1), random_state=model_config.random_state)
     X_train_balanced, y_train_balanced = sm.fit_resample(X_train, y_train)
     
     print("Distribución de clases en y_train después de la agrupación:")
